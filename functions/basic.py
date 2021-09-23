2eac85ad6f75ea42850395abc42c5b32d68258df
demetriomagatti <demetriomagatti@gmail.com>
2021-09-23 17:54:22 +0200

    added comments; minor fix in numpy find_neighbours for better code readability

--- functions/basic.py
+++ functions/basic.py
@@ -7,6 +7,12 @@ rc = 4.625
 rp = 4.375
 
 
+'''
+First idea: read file and save results in pandas dataframe. Index = atom index, three columns for x, y, z coordinates.
+Way too slow when it comes to finding neighbours, as an iteration over rows becomes necessary.
+'''
+
+
 def read_file_pandas(filename):
     atoms_position = pd.read_csv(filename, sep='\s+',header=0,names=['x','y','z'])
     return atoms_position
@@ -23,7 +29,11 @@ def find_neighbours_pandas(atoms_position,rc):
     return neighbours
 
 
-############################################################################################################################
+'''
+Numpy approach. A bit tricky to understand, but way faster. On the same PC:
+    Pandas approach computing time (find_neighbours): approx 0.8 seconds on average
+    Numpy approach computing time (find_neighbours): approx 0.015 seconds on average (53x faster)
+'''
 
 
 def read_file(filename):
@@ -38,13 +48,10 @@ def read_file(filename):
     return n_atoms,sx,sy,sz,x,y,z
     
 
-def find_neighbours(nat,sx,sy,sz,x,y,z,PBC=False):
-    fake_x = np.zeros(nat)
-    fake_y = np.zeros(nat)
-    fake_z = np.zeros(nat)
-    coord_x =  np.column_stack((x,fake_y,fake_z))
-    coord_y =  np.column_stack((fake_x,y,fake_z))
-    coord_z =  np.column_stack((fake_x,fake_y,z))
+def find_neighbours(n_atoms,sx,sy,sz,x,y,z,PBC=False):
+    coord_x =  np.column_stack((x,np.zeros(n_atoms),np.zeros(n_atoms)))
+    coord_y =  np.column_stack((np.zeros(n_atoms),y,np.zeros(n_atoms)))
+    coord_z =  np.column_stack((np.zeros(n_atoms),np.zeros(n_atoms),z))
     x_distances = np.sum(coord_x[:,None,:] - coord_x, axis=-1)
     y_distances = np.sum(coord_y[:,None,:] - coord_y, axis=-1)        
     z_distances = np.sum(coord_z[:,None,:] - coord_z, axis=-1)    
@@ -55,8 +62,8 @@ def find_neighbours(nat,sx,sy,sz,x,y,z,PBC=False):
         mask_y_minus = y_distances<(-0.5*sy)
         x_distances = x_distances - sx*mask_x_plus + sx*mask_x_minus
         y_distances = y_distances - sy*mask_y_plus + sy*mask_y_minus
-    neighbours_distances = np.sqrt((x_distances**2 + y_distances**2 + z_distances**2))
-    mask1 = neighbours_distances<rc
-    mask2 = neighbours_distances>0
+    distances = np.sqrt((x_distances**2 + y_distances**2 + z_distances**2))
+    mask1 = distances<rc
+    mask2 = distances>0
     mask = mask1 * mask2
-    return mask,neighbours_distances
+    return coord_x,mask,distances
