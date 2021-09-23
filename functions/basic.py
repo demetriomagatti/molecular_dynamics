f961d083a485293e3a9084c481e4e71bc6bf4c65
demetriomagatti <demetriomagatti@gmail.com>
2021-09-23 17:39:37 +0200

    added find neighbours pandas and numpy versions

--- functions/basic.py
+++ functions/basic.py
@@ -2,10 +2,61 @@ import numpy as np
 import pandas as pd
 
 
-def read_file(filename):
+# constant values
+rc = 4.625
+rp = 4.375
+
+
+def read_file_pandas(filename):
     atoms_position = pd.read_csv(filename, sep='\s+',header=0,names=['x','y','z'])
     return atoms_position
 
 
-def find_neighbours(atom_position):
-    return 
+def find_neighbours_pandas(atoms_position,rc):
+    neighbours = []
+    for ix,row in tqdm(enumerate(atoms_position.to_numpy())):
+        this_neighbours = []
+        for ix2,row2 in enumerate(atoms_position.to_numpy()):
+            if (ix != ix2) & (np.sqrt(np.sum((np.abs(row-row2)**2)))<rc):
+                this_neighbours.append(ix2)
+        neighbours.append(this_neighbours)
+    return neighbours
+
+
+############################################################################################################################
+
+
+def read_file(filename):
+    x,y,z = np.genfromtxt(filename, unpack=True)
+    sx = x[0]
+    sy = y[0]
+    sz = z[0]
+    x = x[1:]
+    y = y[1:]
+    z = z[1:]
+    n_atoms = len(x)
+    return n_atoms,sx,sy,sz,x,y,z
+    
+
+def find_neighbours(nat,sx,sy,sz,x,y,z,PBC=False):
+    fake_x = np.zeros(nat)
+    fake_y = np.zeros(nat)
+    fake_z = np.zeros(nat)
+    coord_x =  np.column_stack((x,fake_y,fake_z))
+    coord_y =  np.column_stack((fake_x,y,fake_z))
+    coord_z =  np.column_stack((fake_x,fake_y,z))
+    x_distances = np.sum(coord_x[:,None,:] - coord_x, axis=-1)
+    y_distances = np.sum(coord_y[:,None,:] - coord_y, axis=-1)        
+    z_distances = np.sum(coord_z[:,None,:] - coord_z, axis=-1)    
+    if PBC:
+        mask_x_plus = x_distances>(0.5*sx)
+        mask_y_plus = y_distances>(0.5*sy)
+        mask_x_minus = x_distances<(-0.5*sx)
+        mask_y_minus = y_distances<(-0.5*sy)
+        x_distances = x_distances - sx*mask_x_plus + sx*mask_x_minus
+        y_distances = y_distances - sy*mask_y_plus + sy*mask_y_minus
+    neighbours_distances = np.sqrt((x_distances**2 + y_distances**2 + z_distances**2))
+    mask1 = neighbours_distances<rc
+    mask2 = neighbours_distances>0
+    mask = mask1 * mask2
+    return mask,neighbours_distances
--- functions/interaction.py
+++ functions/interaction.py
@@ -4,7 +4,7 @@ import scipy
 import pylab as pp
 
 
-#constant values 
+# constant values 
 rc = 4.625
 rp = 4.375
 sigma = 2.644
@@ -14,7 +14,7 @@ Temp = 100
 m_ag = 108*1.66e-27/16
 
 
-#lennard jones poly7 approximation coefficients
+# lennard jones poly7 approximation coefficients
 Apoli7 = (1/((rc - rp)**7*rp**12))*4*epsilon*rc**4*sigma**6*(2* rp**6 *(-42* rc**3 + 182* rc**2* rp - 273* rc* rp**2 + 143* rp**3) + (455* rc**3 - 1729* rc**2* rp + 2223* rc* rp**2 - 969* rp**3)*sigma**6)
 Bpoli7 = (1/((rc - rp)**7* rp**13))*16* epsilon* rc**3* sigma**6*(rp**6* (54* rc**4 - 154* rc**3* rp + 351* rc *rp**3 - 286* rp**4) + (-315* rc**4 + 749 *rc**3 * rp + 171 *rc**2* rp**2 - 1539* rc* rp**3 + 969* rp**4)* sigma**6)
 Cpoli7 = (1/((rc - rp)**7* rp**14))*12* epsilon* rc**2* sigma**6* (rp**6* (-63* rc**5 - 7* rc**4 *rp + 665* rc**3 *rp**2 - 975* rc**2* rp**3 - 52* rc* rp**4 + 572* rp**5) +  2 *(195* rc**5 + 91* rc**4* rp - 1781* rc**3* rp**2 + 1995 *rc**2* rp**3 + 399* rc* rp**4 - 969* rp**5)* sigma**6)
@@ -25,7 +25,7 @@ Gpoli7 = (1/((rc - rp)**7* rp**15))*4* epsilon* sigma**6* (rp**6* (-224* rc**4 +
 Hpoli7 = (1/((rc - rp)**7* rp**15))*16* epsilon* sigma**6* (rp**6*(14* rc**3 - 63* rc**2* rp + 99* rc* rp**2 - 55* rp**3) + (-91* rc**3 + 351* rc**2* rp - 459* rc* rp**2 + 204* rp**3)* sigma**6)
 
 
-#total potential energy
+# total potential energy
 def lennard_jones():
     '''
     returns total potential energy value
